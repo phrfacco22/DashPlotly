e657e3376cca04005ce015d653114f5434a31dd5
unset <phrfacco99@gmail.com>
2024-06-26 17:03:29 -0300

    "Atualização do dash"

--- plot_csv.py
+++ plot_csv.py
@@ -5,31 +5,61 @@ from dash.dependencies import Input, Output
 from plotly.subplots import make_subplots
  
 
-x = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
-y_bar = [10, 15, 7, 10, 17, 15, 14, 20, 16, 19, 15, 17]
-y_area = [12, 13, 10, 14, 15, 13, 16, 18, 15, 17, 14, 16]
+excel_data = pd.read_excel('csv_plot_valores.xlsx', sheet_name='GRAFICO - VALOR')
+excel_data_qtd = pd.read_excel('csv_plot_valores.xlsx', sheet_name='GRAFICO - QUANTIDADE')
 
-df_orgaos = pd.DataFrame(data=dict(x=y_bar, y=y_area))
- 
+df_orgaos = pd.DataFrame(data=excel_data)
+df_valores = pd.DataFrame(data=excel_data_qtd)
+
+df_orgaos = df_orgaos.loc[2:25]
+df_orgaos['Unnamed: 1'] = pd.to_numeric(df_orgaos['Unnamed: 1'], errors='coerce')
+df_orgaos['Unnamed: 2'] = pd.to_numeric(df_orgaos['Unnamed: 2'], errors='coerce')
+
+df_valores = df_valores.loc[1:20]
+df_valores['Unnamed: 1'] = pd.to_numeric(df_valores['Unnamed: 1'], errors='coerce')
+df_valores['Unnamed: 2'] = pd.to_numeric(df_valores['Unnamed: 2'], errors='coerce')
+
+print(df_valores)
+
 fig = make_subplots(rows=1, cols=1)
 
 fig.add_trace(go.Bar(
-    x=y_area,     
-    y=y_bar,
-    # customdata=
-    text=x,  
-    textposition='auto',
-    marker=dict(opacity=0.7),
-    hovertemplate=
-    '<b>%{text}</b>'+
-    '<b>%</b>',
-    visible=True
+    x=df_orgaos['Unnamed: 1'],     
+    y=df_orgaos['Unnamed: 2'],
+    # customdata=df_orgaos['Unnamed: 1', 'Unnamed: 2'].values,
+    # text=df_orgaos['Unnamed: 0'],  
+    # hovertemplate=
+    # '<b>%{text}</b><br>'+
+    # '<b>Soma de Saldo Inicial: </b> %{customdata[0]}<br>'+
+    # '<b>Soma de Saldo Final: </b> %{customdata[1]}<br>',
+    visible=True,
+    # name=df_orgaos['Unnamed: 0']
 ))
 
 fig.update_layout(
-    title="Gráfico",
-    xaxis_title="",
-    yaxis_title="",
+    title="Gráfico de Entradas x Saídas",
+    xaxis_title="Entradas",
+    yaxis_title="Saídas",
+    showlegend=True
+)
+
+fig.add_trace(go.Bar(
+    x=df_valores['Unnamed: 1'],     
+    y=df_valores['Unnamed: 2'],
+    # customdata=df_orgaos['Unnamed: 1', 'Unnamed: 2'].values,
+    # text=df_orgaos['Unnamed: 0'],  
+    # hovertemplate=
+    # '<b>%{text}</b><br>'+
+    # '<b>Soma de Saldo Inicial: </b> %{customdata[0]}<br>'+
+    # '<b>Soma de Saldo Final: </b> %{customdata[1]}<br>',
+    visible=True,
+    # name=df_orgaos['Unnamed: 0']
+))
+
+fig.update_layout(
+    title="Gráfico de Entradas x Saídas",
+    xaxis_title="Entradas",
+    yaxis_title="Saídas",
     showlegend=True
 )
 
@@ -38,34 +68,34 @@ app = Dash()
 app.layout = html.Div([
     
     
-    dcc.Graph(id='scatter-plot', figure=fig),
+    dcc.Graph(id='bar-plot', figure=fig),
     
-    html.Label("Filtro de  (Eixo X)"),
+    html.Label("Filtro de Valor de Entradas (Eixo X)"),
     dcc.RangeSlider(
     id='x-range-slider',
-    min=df_orgaos.min(),
-    max=df_orgaos.max(),
+    min=df_orgaos['Unnamed: 1'].min(),
+    max=df_orgaos['Unnamed: 1'].max(),
     step=0.1,
     marks=None,
-    value=[df_orgaos.min(), df_orgaos.max()],
+    value=[df_orgaos['Unnamed: 1'].min(), df_orgaos['Unnamed: 1'].max()],
     tooltip={"placement": "bottom", "always_visible": False, "style": {"color": "LightSteelBlue", "fontSize": "10px"}}
     ),
     
-    html.Label("Filtro de  (Eixo Y)"),
+    html.Label("Filtro de Valor de Saídas (Eixo Y)"),
     dcc.RangeSlider(
     id='y-range-slider',
-    min=df_orgaos.min(),
-    max=df_orgaos.max(),
+    min=df_orgaos['Unnamed: 2'].min(),
+    max=df_orgaos['Unnamed: 2'].max(),
     step=0.1,
     marks=None,
-    value=[df_orgaos.min(), df_orgaos.max()],
+    value=[df_orgaos['Unnamed: 2'].min(), df_orgaos['Unnamed: 2'].max()],
     tooltip={"placement": "bottom", "always_visible": False, "style": {"color": "LightSteelBlue", "fontSize": "10px"}}
     )
 ])
 
 @app.callback(
     
-    Output('scatter-plot', 'figure'),
+    Output('bar-plot', 'figure'),
     [Input('x-range-slider', 'value'),
     Input('y-range-slider', 'value')]
 )
@@ -74,24 +104,40 @@ def update_graph(x_range, y_range):
     
     traces = []
             
-    scatter = go.Bar(
-        x=y_area,     
-        y=y_bar,
-        # customdata=
-        text=x,  
+    bar = go.Bar(
+        x=df_orgaos['Unnamed: 1'],     
+        y=df_orgaos['Unnamed: 2'],
+        # customdata=df_orgaos['Unnamed: 1', 'Unnamed: 2'].values,
+        # text=df_orgaos['Unnamed: 0'],  
         textposition='auto',
-        marker=dict(opacity=0.7),
-        hovertemplate=
-        '<b>%{text}</b>'+
-        '<b>%</b>',
+        # hovertemplate=
+        # '<b>%{text}</b><br>'+
+        # '<b>Soma de Saldo Inicial: </b> %{customdata[0]}<br>'+
+        # '<b>Soma de Saldo Final: </b> %{customdata[1]}<br>',
         visible=True,
+        # name=
     )
-    traces.append(scatter)
+    traces.append(bar)
+
+    bar_qtd = go.Bar(
+        x=df_valores['Unnamed: 1'],     
+        y=df_valores['Unnamed: 2'],
+        # customdata=df_orgaos['Unnamed: 1', 'Unnamed: 2'].values,
+        # text=df_orgaos['Unnamed: 0'],  
+        textposition='auto',
+        # hovertemplate=
+        # '<b>%{text}</b><br>'+
+        # '<b>Soma de Saldo Inicial: </b> %{customdata[0]}<br>'+
+        # '<b>Soma de Saldo Final: </b> %{customdata[1]}<br>',
+        visible=True,
+        # name=
+    )
+    traces.append(bar_qtd)
     
     layout = go.Layout(
-        title="Gráfico de Barras",
-        xaxis_title="",
-        yaxis_title="",
+        title="Gráfico de Entradas x Saídas",
+        xaxis_title="Entradas",
+        yaxis_title="Saídas",
         showlegend=True
     )
     
